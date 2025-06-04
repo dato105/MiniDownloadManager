c8c8648474deca6748cb8a9874b36260773431b9
David Sharabi <dato105@gmail.com>
2025-06-04 14:18:11 +0300

    Fixed structure of models and convensions and create DataService to fetch and parse the json

---
+++ DataService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using MiniDownloadManager.Models;
+
+namespace MiniDownloadManager.Services
+{
+    public class DataService
+    {
+        private readonly string jsonUrl;
+        private readonly HttpClient httpClient;
+
+        public DataService(string url)
+        {
+            
+        }
+
+       
+    }
+}
--- File.cs
+++ File.cs
@@ -1,8 +1,27 @@
-public class File
+namespace MiniDownloadManager.Models
 {
-    public string Title { get; set; }
-    public string ImageURL { get; set; }
-    public string FileURL { get; set; }
-    public int Score { get; set; }
-    public Validator validator { get; set; }
+    public class Validator
+    {
+
+        public int disk { get; set; }
+        public int os { get; set; }
+        public int ram { get; set; }
+
+        public Validator(int diskParam , int osParam , int ramParam )
+        {
+            this.disk = diskParam;
+            this.os = osParam;
+            this.ram = ramParam;
+        }
+    }
+
+    public class File
+    {
+        public string Title { get; set; }
+        public string ImageURL { get; set; }
+        public string FileURL { get; set; }
+        public int Score { get; set; }
+        public Validator Validator { get; set; }
+    }
+   
 }
